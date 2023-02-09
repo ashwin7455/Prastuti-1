2565b2ccb20f07c96bd8f8bf345f91a1025716d9
Your Name <you@example.com>
2023-02-09 20:35:23 +0530

    added

--- Client/src/pages/About/AboutPrastuti.js
+++ Client/src/pages/About/AboutPrastuti.js
@@ -5,8 +5,53 @@ export default function AboutPrastuti() {
   return (
     <>
     <Navbar />
-    <section>
-      <h1>ABOUT US</h1>
+    <section className='main'>
+      <h1 className='about'>ABOUT PRASTUTI</h1>
+      <p className='pras'>After 3 long years, we are back with the grandeur events of all time, i.e. Prastuti
+      '23, on the 3rd and 4th of March. We know that you were waiting for it and you
+      know that it's going to be worth the wait!! This time, as KIET is observing its
+      25th year anniversary, Prastuti’23 is being organized with an even bigger and
+      better line-up of inter-institutional cultural competitions. These include a wide
+      mélange of activities, varying from Group Singing, Group Dancing, Band War,
+      Skit, Nukkad Natak, Flash Mob, Fashion Show and other cultural programs to add
+      to the endless amount of entertainment. The fusion of our cultural simplicity
+      and musical melodies that we will have in this golden celebration will definitely
+      be an exceptional experience for everyone. The diverse amalgamation of talent,
+      education, culture, and tradition will surely make Prastuti’23 relishing, ravishing,
+      and worth being a part of.</p>
+       <div className="container">
+
+      <div className='card-1 allcard'>
+       <div className="font-card" >REWARDS &</div>
+       <div className="font-card" >GOODIES WORTH</div>
+       <div className="font-card" >3 LAKH</div>
+      </div>
+
+      <div className='card-2 allcard'>
+      <div className="font-card" >REWARDS &</div>
+       <div className="font-card" >GOODIES WORTH</div>
+       <div className="font-card" >3 LAKH</div>
+      </div>
+
+      <div className='card-3 allcard'>
+      <div className="font-card" >REWARDS &</div>
+       <div className="font-card" >GOODIES WORTH</div>
+       <div className="font-card" >3 LAKH</div>
+      </div>
+
+      <div className='card-4 allcard'>
+      <div className="font-card" >REWARDS &</div>
+       <div className="font-card" >GOODIES WORTH</div>
+       <div className="font-card" >3 LAKH</div>
+      </div>
+
+      <div className='card-5 allcard'>
+      <div className="font-card" >REWARDS &</div>
+       <div className="font-card" >GOODIES WORTH</div>
+       <div className="font-card" >3 LAKH</div>
+      </div>
+
+      </div> 
     </section>
     </>
   )
